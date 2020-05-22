3b4333bb2f97063c9ec9e6e81ae789e5c209df3e
Your Name <you@example.com>
2020-05-22 14:04:32 +0300

    fix

--- src/Models/ChatMessage.php
+++ src/Models/ChatMessage.php
@@ -7,13 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class ChatMessage extends Model
 {
     protected $guarded = ['id'];
-//    public function chat()
-//    {
-//        return $this->belongsTo('MarksIhor\LaravelMessaging\Models\Chat');
-//    }
-//
-//    public function user()
-//    {
-//        return $this->belongsTo(config('auth.providers.users.model'));
-//    }
+
+    protected $hidden = ['updated_at', 'user_id', 'chat_id'];
 }
